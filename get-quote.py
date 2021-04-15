1a8d086a0032b749d24bba033b48d3664966be06
Trevor <tpshaw@gmail.com>
2021-04-15 09:31:28 -0600

    added more than one quote

--- get-quote.py
+++ get-quote.py
@@ -2,15 +2,21 @@ import random
 
 def mine():
 
-
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-	
-  print(quotes[rnd])
+  #last = len(quotes) - 1
+  rnd_one = randomize(quotes)
+  rnd_two = randomize(quotes)	
+  print(quotes[rnd_one])
+  print(quotes[rnd_two])
+  
+def randomize(quotes):
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)	
 
+  return rnd
+  
 if __name__== "__main__":
   mine()
